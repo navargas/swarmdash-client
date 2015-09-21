9d6c217a6817991a119c3893a8cef69c64228e86
Nicholas Vargas <mail@nickvargas.net>
2015-09-20 22:16:49 -0700

    Add template mqtt publish code

--- swarmdash.py
+++ swarmdash.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python
 import ibmiotf.device
+import uuid
 
-print('OK')
+ID = str(uuid.uuid4()).replace('-','')
+ID=  'wdiaiodioasdojbobobo'
+iotclient = None
+
+try:
+  options = {
+    'org': 'quickstart',
+    'type': 'quickstart',
+    'id': ID,
+    'auth-method':'quickstart',
+    'auth-token':''
+  }
+  iotclient = ibmiotf.device.Client(options)
+except ibmiotf.ConnectionException  as e:
+  print('There was an issue')
+  print(e)
+
+
+iotclient.connect()
+
+iotclient.publishEvent('event', 'json', {'name1':{}})
